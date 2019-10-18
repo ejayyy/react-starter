08ef6053562715a9d52d6dec545883b14a417005
ejayyy <ejay0811@gmail.com>
2019-10-17 21:54:07 -0700

    [add] status

--- src/js/Board.js
+++ src/js/Board.js
@@ -8,6 +8,7 @@ class Board extends React.Component {
     render() {
         return (
             <div>
+                <h2>Next player: X</h2>
                 <div>
                     {this.renderSquare(0)}
                     {this.renderSquare(1)}
--- src/js/Square.js
+++ src/js/Square.js
@@ -2,10 +2,16 @@ import React from 'react'
 import '../css/custom.css'
 
 class Square extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: null,
+        };
+    }
     render() {
         return (
-            <button className="square">
-                {this.props.value}
+            <button className="square" onClick={() => this.setState({value: 'X'})}>
+                {this.state.value}
             </button>
         );
     }

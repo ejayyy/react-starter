4899a445d6e6af898ddc16405d1069c080e19e24
ejayyy <ejay0811@gmail.com>
2019-10-18 21:38:54 -0700

    [add] status

--- src/js/Board.js
+++ src/js/Board.js
@@ -2,13 +2,27 @@ import React from 'react'
 import Square from './Square'
 
 class Board extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            squares: Array(9).fill(null)
+        }
+    }
+    handleClick(i) {
+        const squares = this.state.squares.slice();
+        squares[i] = 'X';
+        this.setState({ squares: squares });
+    }
     renderSquare(i) {
-        return <Square value={i} />;
+        return <Square value={this.state.squares[i]}
+            onClick={() => this.handleClick(i)}
+        />;
     }
     render() {
+        const status = 'Next player: X'
         return (
             <div>
-                <h2>Next player: X</h2>
+                <div className="status">{status}</div>
                 <div>
                     {this.renderSquare(0)}
                     {this.renderSquare(1)}
